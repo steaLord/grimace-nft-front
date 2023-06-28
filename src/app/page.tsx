c207e965dfa04b905414703ef7f8569ab082235d
evermake <vladeryabkin@gmail.com>
2023-06-28 22:46:23 +0300

    feat: add heading and image on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,69 @@
 "use client";
-import GrimaceMandalasNFT from "@/nftArtifacts/GrimaceMandalaNFT.json";
-import { useMetaMask } from "metamask-react";
-import { StyledButton } from "@/components/Header/Header";
-import { Web3 } from "web3";
 
-// TODO: Set contract address from proccess.env.CONTRACT_ADDRESS
-export const contractAddress = "0x1C5e8f0fa8B15E735dAd516146A56366c5469438";
+import styled from "@emotion/styled";
+import Container from "@/components/Container";
+import Image from "next/image";
+import grimaceNFTImage from "./grimace-nft-image.png";
 
-const useMintNFT = (contractAddress) => {
-  const { account } = useMetaMask();
+const Root = styled(Container)`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
 
-  const handleMintNFT = async (uri) => {
-    try {
-      if (!account) {
-        console.error("Web3 provider or account not available");
-        return;
-      }
+const Content = styled.div``;
 
-      const web3 = new Web3(window.ethereum);
-      // Create an instance of the contract using the contract ABI and address
-      const contract = new web3.eth.Contract(
-        GrimaceMandalasNFT.abi,
-        contractAddress,
-      );
+const Subheading = styled.h2`
+  font-size: 2rem;
+  font-weight: 300;
+`;
 
-      // Call the safeMint function to mint the NFT
-      await contract.methods.safeMint(account, uri).send({
-        from: account,
-      });
+const NFTImage = styled(Image)`
+  width: 500px;
+  max-width: 50%;
+  height: auto;
+`;
 
-      // NFT minted successfully
-      console.log("NFT minted successfully!");
-    } catch (error) {
-      console.error("Error minting NFT:", error);
-    }
-  };
+function NFT() {
+  return <NFTImage src={grimaceNFTImage} alt="Grimace NFT"/>;
+}
 
-  return { account, handleMintNFT };
-};
+const HeadingWrapper = styled.h1`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 14px;
+`;
+const HeadingPart1 = styled.span`
+  font-size: 64px;
+  font-weight: 400;
+  line-height: 1;
+`;
+const HeadingPart2 = styled.span`
+  font-size: 96px;
+  font-weight: 700;
+  text-transform: uppercase;
+  line-height: 0.8;
+`;
+const HeadingPart3 = styled.span`
+  font-size: 210px;
+  font-weight: 700;
+  text-transform: uppercase;
+  line-height: 0.8;
+`;
+
 
 export default function Home() {
-  const { handleMintNFT } = useMintNFT(contractAddress);
   return (
-    <>
-      <StyledButton onClick={() => handleMintNFT("ssoto")}>
-        Mint NFT
-      </StyledButton>
-    </>
+    <Root>
+      <Content>
+        <HeadingWrapper>
+          <HeadingPart1>Meet</HeadingPart1>
+          <HeadingPart2>Grimace</HeadingPart2>
+          <HeadingPart3>NFT</HeadingPart3>
+        </HeadingWrapper>
+        <Subheading>The first collection with sense</Subheading>
+      </Content>
+      <NFT/>
+    </Root>
   );
 }
