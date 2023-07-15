de1332494cc0b78538fbfd49aed6e65e210515ee
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-07-15 21:28:47 +0100

    feat(app): update release date

--- src/components/Countdown/Countdown.tsx
+++ src/components/Countdown/Countdown.tsx
@@ -2,24 +2,64 @@ import { HTMLAttributes } from "react";
 import styled from "@emotion/styled";
 import CountdownBox from "@/components/Countdown/CountdownBox";
 
-
 export type CountdownProps = HTMLAttributes<HTMLDivElement> & {
   num1: number;
   num2: number;
   num3: number;
-}
+};
+
+const CountdownBoxRoot = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const CountdownTimeWrapper = styled.div`
+  display: flex;
+  div:first-child {
+    margin-right: 8px;
+  }
+`;
+
+const CountDownBoxWrapper = ({ text1, text2, label }) => {
+  return (
+    <CountdownBoxRoot>
+      <CountdownTimeWrapper>
+        <CountdownBox text={text1} />
+        <CountdownBox text={text2} />
+      </CountdownTimeWrapper>
+      <div>{label}</div>
+    </CountdownBoxRoot>
+  );
+};
 
 function Countdown({ num1, num2, num3, ...rest }: CountdownProps) {
   return (
     <Root {...rest}>
-      <CountdownBox text={(Math.floor(num1 / 10) % 10).toString()}/>
-      <CountdownBox text={(num1 % 10).toString()}/>
+      <CountDownBoxWrapper
+        text2={(num1 % 10).toString()}
+        text1={(Math.floor(num1 / 10) % 10).toString()}
+        label="Days"
+      />
       <Separator>:</Separator>
-      <CountdownBox text={(Math.floor(num2 / 10) % 10).toString()}/>
-      <CountdownBox text={(num2 % 10).toString()}/>
+      <CountDownBoxWrapper
+        text2={(num2 % 10).toString()}
+        text1={(Math.floor(num2 / 10) % 10).toString()}
+        label="Hours"
+      />
       <Separator>:</Separator>
-      <CountdownBox text={(Math.floor(num3 / 10) % 10).toString()}/>
-      <CountdownBox text={(num3 % 10).toString()}/>
+      <CountDownBoxWrapper
+        text2={(num3 % 10).toString()}
+        text1={(Math.floor(num3 / 10) % 10).toString()}
+        label="Minutes"
+      />
+      {/*<CountdownBoxRoot>
+        <CountdownTimeWrapper>
+          <CountdownBox text={(Math.floor(num1 / 10) % 10).toString()} />
+          <CountdownBox text={(num1 % 10).toString()} />
+        </CountdownTimeWrapper>
+        <div>Days</div>
+      </CountdownBoxRoot>*/}
     </Root>
   );
 }
@@ -38,7 +78,7 @@ const Root = styled.div`
 const Separator = styled.span`
   font-size: 80px;
   line-height: 1;
-  transform: translateY(-13%); // Font line height fix
+  transform: translateY(-25%); // Font line height fix
 
   @media (max-width: 768px) {
     font-size: 60px;
