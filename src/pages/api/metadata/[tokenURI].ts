f01edb98b91a4acb7d5cae6249c4d4eecfd1e165
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-07-16 14:57:17 +0100

    feat(app): update token URI data

--- src/pages/api/metadata/[tokenURI].ts
+++ src/pages/api/metadata/[tokenURI].ts
@@ -10,31 +10,37 @@ import nftsMetadata from "/public/NFTsMetadata.json";
 const metadata = {
   "SAFE4LIFE.COM": {
     id: "eternal-supreme-ultradegen",
+    // id: "1",
     collection: "Eternal Supreme UltraDegen",
     imageSrc: nft1Src,
   },
   "TEST_LINK.COM": {
     id: "vip-degen-highest-caste",
+    // id: "12",
     collection: "VIP-Degen Highest Caste",
     imageSrc: nft2Src,
   },
   rgoto: {
     id: "reverd-guardian-of-the-odyssey",
+    // id: "28",
     collection: "Revered Guardian of the Odyssey",
     imageSrc: nft3Src,
   },
   bdd: {
     id: "believer-diamond-degen",
+    // id: "35",
     collection: "Believer Diamond Degen",
     imageSrc: nft4Src,
   },
   gdd: {
     id: "golden-degen-dick",
+    // id: "46",
     collection: "Golden Degen Dick",
     imageSrc: nft5Src,
   },
   ssoto: {
     id: "silver-soldier-of-the-odyssey",
+    // id: "58",
     collection: "Silver Soldier of the Odyssey",
     imageSrc: nft6Src,
   },
