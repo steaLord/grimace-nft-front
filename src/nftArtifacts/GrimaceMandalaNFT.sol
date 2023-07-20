85c0b56aba27bb07c4d685f25d0daec60c353895
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-07-20 18:44:11 +0100

    feat(sc): update

--- src/nftArtifacts/GrimaceMandalaNFT.sol
+++ src/nftArtifacts/GrimaceMandalaNFT.sol
@@ -10,6 +10,10 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+/**
+ * @title GrimaceMandalaNFT contract
+ * @dev ERC721 token contract for Grimace Mandala NFTs with auction functionality.
+ */
 contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
@@ -31,19 +35,34 @@ contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausab
 
     mapping(uint256 => Auction) private tokenIdToAuction;
 
+    /**
+     * @dev Contract constructor.
+     * @param _grimaceCoinAddress Address of the Grimace Coin ERC20 token.
+     */
     constructor(address _grimaceCoinAddress) ERC721("GrimaceMandalaNFT", "GMNFT") {
         grimaceCoinAddress = _grimaceCoinAddress;
         grimaceCoin = IERC20(_grimaceCoinAddress);
     }
 
+    /**
+     * @dev Pauses all token transfers.
+     */
     function pause() public onlyOwner {
         _pause();
     }
 
+    /**
+     * @dev Unpauses token transfers.
+     */
     function unpause() public onlyOwner {
         _unpause();
     }
 
+    /**
+     * @dev Safely mints a new NFT and assigns it to the given address.
+     * @param to Address to which the NFT will be minted.
+     * @param uri URI of the token metadata.
+     */
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -53,11 +72,22 @@ contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausab
         sequentialCounter++;
     }
 
-    // Used to get auction data from frontend without need of owning nft.
+    /**
+     * @dev Retrieves the token ID associated with the given sequential ID.
+     * @param sequentialId Sequential ID of the token.
+     * @return tokenId The corresponding token ID.
+     */
     function getNFTBySequentialId(uint256 sequentialId) public view returns (uint256) {
         return sequentialToTokenId[sequentialId];
     }
 
+    /**
+     * @dev Starts an auction for the specified token.
+     * @param tokenId Token ID for which the auction will be started.
+     * @param initialPrice Initial price for the auction.
+     * @param bidStep Bid increment for each bid.
+     * @param auctionDurationInDays Duration of the auction in days.
+     */
     function startAuction(
         uint256 tokenId,
         uint256 initialPrice,
@@ -74,6 +104,34 @@ contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausab
         tokenIdToAuction[tokenId] = newAuction;
     }
 
+    /**
+     * @dev Ends an ongoing auction for the specified token.
+     * @param tokenId Token ID for which the auction will be ended.
+     */
+    function endAuction(uint256 tokenId) public onlyOwner {
+        Auction storage auction = tokenIdToAuction[tokenId];
+        require(auction.endTime > 0, "Auction does not exist for this token");
+        require(block.timestamp >= auction.endTime, "Auction has not ended yet");
+
+        if (auction.highestBidder != address(0)) {
+            // Transfer the token to the highest bidder
+            _transfer(ownerOf(tokenId), auction.highestBidder, tokenId);
+            // Pay the seller the auction amount minus fee
+            uint256 fee = auction.highestBid * 2 / 100; // 2% fee
+            require(grimaceCoin.transfer(owner(), auction.highestBid - fee), "Transfer failed");
+            // Pay the fee to the contract owner
+            require(grimaceCoin.transfer(owner(), fee), "Transfer failed");
+        }
+
+        // Clear the auction data
+        delete tokenIdToAuction[tokenId];
+    }
+
+    /**
+     * @dev Places a bid on the specified token auction.
+     * @param tokenId Token ID for which the bid is placed.
+     * @param bidAmount Bid amount in Grimace Coin.
+     */
     function placeBid(uint256 tokenId, uint256 bidAmount) public {
         Auction storage auction = tokenIdToAuction[tokenId];
         require(auction.endTime > 0, "Auction does not exist for this token");
@@ -81,44 +139,61 @@ contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausab
         require(bidAmount >= auction.highestBid + auction.bidStep, "Bid amount must be greater than current highest bid");
 
         if (auction.highestBidder != address(0)) {
-            // Refund the previous highest bidder if exists
+            // Refund the previous highest bidder
             require(grimaceCoin.transfer(auction.highestBidder, auction.highestBid), "Failed to refund previous highest bidder");
         }
 
+        // Transfer the bid amount from the bidder to the contract
         require(grimaceCoin.transferFrom(msg.sender, address(this), bidAmount), "Failed to transfer bid amount");
+
+        // Update auction data with the new highest bidder and bid amount
         auction.highestBidder = msg.sender;
         auction.highestBid = bidAmount;
     }
 
+    /**
+     * @dev Withdraws the remaining Grimace Coin funds from the contract to the owner's address.
+     */
     function withdrawFunds() public onlyOwner {
         uint256 contractBalance = grimaceCoin.balanceOf(address(this));
         require(grimaceCoin.transfer(owner(), contractBalance), "Transfer failed");
     }
 
+    /**
+     * @dev Retrieves the details of an auction for the specified token.
+     * @param tokenId Token ID for which the auction details are retrieved.
+     * @return Auction details including token ID, initial price, bid step, end time, highest bidder, and highest bid.
+     */
     function getAuctionDetails(uint256 tokenId)
     public
     view
     returns (
-        uint256 tokenId,
-        uint256 initialPrice,
-        uint256 bidStep,
-        uint256 endTime,
-        address highestBidder,
-        uint256 highestBid
+        uint256,
+        uint256,
+        uint256,
+        uint256,
+        address,
+        uint256
     )
     {
         Auction memory auction = tokenIdToAuction[tokenId];
-
         return (
-            auction.tokenId,
-            auction.initialPrice,
-            auction.bidStep,
-            auction.endTime,
-            auction.highestBidder,
-            auction.highestBid
+        auction.tokenId,
+        auction.initialPrice,
+        auction.bidStep,
+        auction.endTime,
+        auction.highestBidder,
+        auction.highestBid
         );
     }
 
+    /**
+     * @dev Hook function called before any token transfer.
+     * @param from Address from which the token is transferred.
+     * @param to Address to which the token is transferred.
+     * @param tokenId Token ID being transferred.
+     * @param batchSize Number of tokens being transferred.
+     */
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
     internal
     whenNotPaused
@@ -127,16 +202,28 @@ contract GrimaceMandalaNFT is ERC721, ERC721URIStorage, ERC721Enumerable, Pausab
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-    // The following functions are overrides required by Solidity.
-
+    /**
+     * @dev Hook function called before token burn.
+     * @param tokenId Token ID being burned.
+     */
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
+    /**
+     * @dev Retrieves the token URI for the specified token ID.
+     * @param tokenId Token ID for which the URI is retrieved.
+     * @return The token URI.
+     */
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
+    /**
+     * @dev Checks if the contract supports the specified interface.
+     * @param interfaceId Interface ID being checked.
+     * @return Whether the interface is supported or not.
+     */
     function supportsInterface(bytes4 interfaceId)
     public
     view
