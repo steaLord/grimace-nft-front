97451eff21f82714e2d8407acea6a1f47e079099
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-07-26 17:22:38 +0100

    feat(useAuction): more safe checks for placeBid function

--- src/app/hooks/useAuction.ts
+++ src/app/hooks/useAuction.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useMetaMask } from "metamask-react";
 import { useWeb3Context } from "@/app/hooks/useWeb3";
+import { toast } from "react-toastify";
 
 export interface IBlockchainAuctionData {
   nftID: number;
@@ -11,6 +12,17 @@ export interface IBlockchainAuctionData {
   highestBidder: string;
   highestBid: number | BigInt;
 }
+
+const getApprovedAmount = async ({
+  tokenContract,
+  account,
+  nftContractAddress,
+}) => {
+  const approvedAmount = BigInt(
+    await tokenContract.methods.allowance(account, nftContractAddress).call()
+  );
+  return { approvedAmount };
+};
 
 export const getAuctionDetails = async ({ nftContract, nftID }) => {
   const auctionDetails = await nftContract?.methods
@@ -100,32 +112,64 @@ const useAuction = ({ nftID }: { nftID: number }) => {
         nftID,
       });
 
+      console.log(currentAuctionDetails.highestBid, auctionDetails.highestBid);
+      if (
+        BigInt(currentAuctionDetails.highestBid) !==
+        BigInt(auctionDetails.highestBid)
+      ) {
+        setAuctionDetails(currentAuctionDetails);
+        toast.error("Current bid has been changed");
+        throw new Error("Current bid has been changed");
+      }
+
       // Convert bidStep to actual value using decimalsMultiplier
       const decimals = await tokenContract.methods.decimals().call();
-      console.log({ decimals });
       const decimalsMultiplier = BigInt(10) ** BigInt(decimals);
 
       // Check if the user has approved the contract to spend tokens on their behalf
-      const approvedAmount = BigInt(
-        await tokenContract.methods
-          .allowance(account, nftContractAddress)
-          .call()
-      );
+      const { approvedAmount } = await getApprovedAmount({
+        tokenContract,
+        account,
+        nftContractAddress,
+      });
 
-      console.log({ approvedAmount });
-
+      // Check user balance to not place bid if there's not enough tokens
       const bidAmount =
         BigInt(currentAuctionDetails.highestBid) === BigInt(0)
           ? BigInt(auctionDetails.initialPrice)
           : BigInt(currentAuctionDetails.highestBid) +
             BigInt(currentAuctionDetails.bidStep);
+      const balance = await tokenContract.methods.balanceOf(account).call();
+      if (balance < bidAmount) {
+        toast.error(`You don't have enough GRIMACE to place bid`);
+        throw new Error(`You don't have enough GRIMACE to place bid`);
+      }
 
-      // if (approvedAmount < bidAmount) {
-      // Convert the bid amount to token units for approval
-      await tokenContract.methods
-        .approve(nftContractAddress, bidAmount.toString())
-        .send({ from: account });
-      // }
+      if (approvedAmount < bidAmount) {
+        await tokenContract.methods
+          .approve(
+            nftContractAddress,
+            (bidAmount / decimalsMultiplier).toString()
+          )
+          .send({ from: account });
+        const { approvedAmount } = await getApprovedAmount({
+          tokenContract,
+          account,
+          nftContractAddress,
+        });
+        if (approvedAmount < bidAmount) {
+          toast.error(
+            `You need to approve to spend minimum ${
+              bidAmount / decimalsMultiplier
+            } tokens`
+          );
+          throw new Error(
+            `You need to approve to spend minimum ${
+              bidAmount / decimalsMultiplier
+            } tokens`
+          );
+        }
+      }
 
       // Call placeBid function on the smart contract
       const nftIdResponse = await nftContract.methods
