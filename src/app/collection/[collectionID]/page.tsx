9ec1e4e16ef7c02daefad7fffe6743a0484a2611
Dzmitry Baranau <dimabaranov98@gmail.com>
2023-07-28 00:54:23 +0100

    feat(collection): show highest bid only if it's exist

--- src/app/collection/[collectionID]/page.tsx
+++ src/app/collection/[collectionID]/page.tsx
@@ -49,12 +49,11 @@ export default function NFTPage() {
                   >
                     <P>{edition}</P>
                   </div>
-                  {highestBid !== 0 &&
-                    highestBid(
-                      <HighestBidContainer>
-                        <HighBidP>{highestBid} $GRIMACE</HighBidP>
-                      </HighestBidContainer>
-                    )}
+                  {highestBid !== 0 && highestBid && (
+                    <HighestBidContainer>
+                      <HighBidP>{highestBid} $GRIMACE</HighBidP>
+                    </HighestBidContainer>
+                  )}
                   {Number(endTime) <=
                     Math.floor(new Date().getTime() / 1000) && (
                     <HighestBidContainer>
